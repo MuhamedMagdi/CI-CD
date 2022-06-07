16fc3c1408b16384663f421883460e525bbf606f
Mohamed Magdi <48320401+MuhamedMagdi@users.noreply.github.com>
2022-06-07 02:11:56 +0000

    adding the api path

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://fwd-api-env.us-east-1.elasticbeanstalk.com",
+  apiHost: "http://fwd-api-env.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
