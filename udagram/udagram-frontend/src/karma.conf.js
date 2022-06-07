ab019f2f9355123a69446b487417104f68594c4a
Mohamed Magdi <48320401+MuhamedMagdi@users.noreply.github.com>
2022-06-07 15:28:26 +0000

    configure chrome in circleci for angular

--- udagram/udagram-frontend/src/karma.conf.js
+++ udagram/udagram-frontend/src/karma.conf.js
@@ -25,7 +25,12 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
